8241413b69dee6ac6e26a7bcd50c7a7653434dd0
Mimmisen <mimmi.fordaluddin@hotmail.com>
2022-03-08 17:02:14 +0100

    started with the project

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import data from './data.json'
+import React from "react";
+import data from "./data.json";
+import { Album } from "components/Album";
 
-console.log(data)
+console.log(data);
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+  const albumArray = data.albums.items.map((item) => {
+    return item;
+  });
+  console.log(albumArray);
+  const albumCover = albumArray.map((album) => {
+    return (image = album.images[1].url);
+  });
+  console.log(albumCover);
+};
---
+++ code/src/components/Album.js
@@ -0,0 +1,13 @@
+// import React from "react";
+
+// import data from './data.json'
+
+// export const Album = (props) => {
+//   return (
+//     <article className='album-cover'>
+//         <div></div>
+//             <img src='{props.}'/>
+//     </article>
+
+//   )
+// };
