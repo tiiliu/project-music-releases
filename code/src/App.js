a3aab30036da6b1cbb76ca6cbac0f904fb57b7d0
Mimmisen <mimmi.fordaluddin@hotmail.com>
2022-03-09 16:58:26 +0100

    we maped all the data for the albums and artists and added icons

--- code/src/App.js
+++ code/src/App.js
@@ -1,27 +1,44 @@
 import React from "react";
 import data from "./data.json";
-import { Album } from "components/Album";
+// import { Album } from "components/Album";
+
+import heart from "./icons/heart.svg";
+import play from "./icons/play.svg";
+import dots from "./icons/dots.svg";
 
 // console.log(data);
 
 export const App = () => {
-  const albumArray = data.albums.items.map((item) => {
-    return {
-      artistArray: item.artists.map((artist) => {
-        return {
-          artistName: artist.name,
-          artistUrl: artist.external_urls.spotify,
-        };
-      }),
-      albumName: item.name,
-      imageAlbum: item.images[1].url,
-    };
-  });
-  console.log("album", albumArray);
-
-  // console.log(albumArray);
-  // const albumCover = albumArray.map((album) => {
-  //   return (image = album.images[1].url);
-  // });
-  // console.log(albumCover);
+  return (
+    <section className="album-container">
+      {data.albums.items.map((album) => (
+        <div className="album-card">
+          <img
+            className="album-cover"
+            src={album.images[1].url}
+            alt="album cover"
+          />
+          <img className="heart-icon" src={heart} alt="heart icon" />
+          <img className="play-icon" src={play} alt="play icon" />
+          <img className="dots-icon" src={dots} alt="dots icon" />
+          <a
+            className="album-link"
+            href={album.external_urls.spotify}
+            target="-blank"
+          >
+            <h2 className="album-name">{album.name}</h2>
+          </a>
+          {album.artists.map((artist) => (
+            <a
+              className="artist-link"
+              href={artist.external_urls.spotify}
+              target="-blank"
+            >
+              <h3 className="artist-name">{artist.name}</h3>
+            </a>
+          ))}
+        </div>
+      ))}
+    </section>
+  );
 };
--- code/src/components/Artist.js
+++ code/src/components/Artist.js
@@ -1,5 +1,5 @@
-import React from "react";
+// import React from "react";
 
-export const Artist = () => {
-  return;
-};
+// export const Artist = () => {
+//   return;
+// };
