06bde2caea1ee245646724f9c8a2265926687deb
Mimmisen <mimmi.fordaluddin@hotmail.com>
2022-03-09 10:54:52 +0100

    returned the data from album, image and artists

--- code/src/App.js
+++ code/src/App.js
@@ -2,15 +2,26 @@ import React from "react";
 import data from "./data.json";
 import { Album } from "components/Album";
 
-console.log(data);
+// console.log(data);
 
 export const App = () => {
   const albumArray = data.albums.items.map((item) => {
-    return item;
+    return {
+      artistArray: item.artists.map((artist) => {
+        return {
+          artistName: artist.name,
+          artistUrl: artist.external_urls.spotify,
+        };
+      }),
+      albumName: item.name,
+      imageAlbum: item.images[1].url,
+    };
   });
-  console.log(albumArray);
-  const albumCover = albumArray.map((album) => {
-    return (image = album.images[1].url);
-  });
-  console.log(albumCover);
+  console.log("album", albumArray);
+
+  // console.log(albumArray);
+  // const albumCover = albumArray.map((album) => {
+  //   return (image = album.images[1].url);
+  // });
+  // console.log(albumCover);
 };
